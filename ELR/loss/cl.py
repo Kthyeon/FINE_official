3050251336ff49b3394b23bb5a7c1dfdc180a631
jaychoi12 <jaychoi@postech.ac.kr>
2020-10-27 15:40:04 +0900

    add curriculum loss

--- ELR/loss/cl.py
+++ ELR/loss/cl.py
@@ -46,23 +46,27 @@ class CLoss(nn.Module):
         # margin for each data point = t_y - max(i!=y)t_i , y is target class num, shape (Batch_size,)
         margin = output[range(len(output)), target] - torch.max(tmp_output, dim=1).values
         
-        v = self.algorithm1(margin, threshold)
+        # parameters required to calculate curriculum loss
+        v = partial_opt(margin, threshold, base_loss)
+        l = base_loss(margin)
+        batch_size = output.shape[0]
         
+        # curriculum loss is maximum value between loss 1 and 2
+        curriculum_loss_1 = torch.dot(v, l)
+        curriculum_loss_2 = batch_size - torch.sum(v) + torch.sum(torch.ones(batch_size)[margin < 0])
         
+        if curriculum_loss_1 < curriculum_loss_2:
+            curriculum_loss = curriculum_loss_2
+        else:
+            curriculum_loss = curriculum_loss_1
         
-    
+        return curriculum_loss
+        
 class SoftHingeLoss(nn.Module):
     def __init__(self):
         super(SoftHingeLoss, self).__init__()
 
-    def forward(self, output, target):
-        tmp_output = output.clone()
-        target = target.long()
-        tmp_output[range(len(output)), target] = float("-inf")
-        
-        # margin for each data point = t_y - max(i!=y)t_i , y is target class num
-        margin = output[range(len(output)), target] - torch.max(tmp_output, dim=1).values
-        
+    def forward(self, margin):
         # soft hinge loss described in the paper elr, appendix H
         if margin >= 0:
             soft_hinge_loss = 1 - margin
@@ -77,16 +81,8 @@ class HardHingeLoss(nn.Module):
     def __init__(self):
         super(HardHingeLoss, self).__init__()
 
-    def forward(self, output, target):
-        tmp_output = output.clone()
-        target = target.long()
-        tmp_output[range(len(output)), target] = float("-inf")
-        
-        # margin for each data point = t_y - max(i!=y)t_i , y is target class num
-        margin = output[range(len(output)), target] - torch.max(tmp_output, dim=1).values
-        
+    def forward(self, margin):
         # hard hinge loss described in the paper elr, appendix H
-        
         hard_hinge_loss = 1 - margin
         hard_hinge_loss[soft_hinge_loss < 0] = 0
         

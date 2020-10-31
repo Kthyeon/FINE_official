77474e11269d1a724f4b69a8d5cbcde09d4d6046
Kthyeon <potter32@kaist.ac.kr>
2020-10-31 23:18:44 +0900

    modify cl

--- ELR/loss/cl.py
+++ ELR/loss/cl.py
@@ -9,19 +9,15 @@ def partial_opt(loss_value, threshold):
     L = 0
     batch_size = len(loss_value)
 #     v_set = torch.nn.Parameter(data=torch.ones(batch_size), requires_grad=False)
-    v_set = torch.ones(batch_size)
+    # v_set = torch.ones(batch_size)
     sorted_loss, index = torch.sort(loss_value) #Sort losses in non-dereasing order
-                        
+    index_list = []
     for i in range(batch_size):
         L += sorted_loss[i]
         if L <= (threshold + 1 - i):
-            v_set[index[i]] = 1
-        else:
-            v_set[index[i]] = 0
-            
-    v_set = v_set.float()
-    
-    return v_set
+            index_list.append(index[i].item())
+                
+    return index_list
 
 def softHingeLoss(margin, output, target): #margin.shape = [batch_size]
     
@@ -36,6 +32,15 @@ def hardHingeLoss(margin): # margin.shape = [batch_size]
     hard_hinge_loss[hard_hinge_loss < 0] = 0
 
     return hard_hinge_loss
+
+
+class Binary(nn.Module):
+    def __init__(self):
+        super().__init__() # init the base class
+        
+    def forward(self, x):
+        
+        return torch.ones_like(x)
 
 
 class NPCLoss(nn.Module):	
@@ -53,42 +58,43 @@ class NPCLoss(nn.Module):
         tmp_output[range(len(output)), target] = float("-inf")
         margin = output[range(len(output)), target] - torch.max(tmp_output, dim=1).values
         
+        
         #calculate threshold
         batch_size = output.shape[0]
-        threshold = ((1 - self.epsilon) ** 2) * batch_size + (1 - self.epsilon) * torch.sum(torch.ones(batch_size)[margin < 0])
-        threshold = int(threshold) #threshold 왜 int???
-
+        threshold = ((1 - self.epsilon) ** 2) * batch_size + (1 - self.epsilon) * torch.sum(margin < 0.)
+         #threshold 왜 int???
+        
         # parameters required to calculate NPCL
         loss_val = softHingeLoss(margin, output, target).to('cuda')
-        v = partial_opt(loss_val, threshold).to('cuda')
+        v_index = partial_opt(loss_val, threshold) #.to('cuda')
 
         # calculate NPCL
-        npcl_1 = torch.dot(v, loss_val).to('cuda')
-        npcl_2 = threshold - torch.sum(v).to('cuda')
+        npcl_1 = torch.sum(loss_val[v_index]).to('cuda')
+        bi = Binary()
+        npcl_2 = threshold - torch.sum(bi(loss_val[v_index])).to('cuda')
         npcl_2.requires_grad=True
         
         loss_final = npcl_2 if npcl_1 < npcl_2 else npcl_1
-#         pdb.set_trace()
-#         loss_val.requires_grad = True
         
-        return loss_final
+        return loss_final / len(v_index)
     
 class CLoss(nn.Module):
     def __init__(self):
         super().__init__()
     
     def forward(self, output, target):
+        
         # margin for each data point = t_y - max(i!=y)t_i , y is target class num, shape (Batch_size,)
-        tmp_output = output.clone()
+        prob_output = F.softmax(output, dim=1)
         target = target.long()
-        tmp_output[range(len(output)), target] = float("-inf")
-        margin = output[range(len(output)), target] - torch.max(tmp_output, dim=1).values
+        # tmp_output[:, target] = float("-inf")
+        margin = prob_output[:, target] - torch.max(prob_output, dim=1).values
         
         # parameters required to calculate curriculum loss
         batch_size = output.shape[0]
         l = softHingeLoss(margin, output, target).to('cuda') # shape = [batch]
         threshold = batch_size # temporarily set to n; it should be 0 <= C <= 2n
-        v = partial_opt(l, threshold).to('cuda') # shape = [batch] / 0 or 1
+        v = partial_opt(l, threshold)# .to('cuda') # shape = [batch] / 0 or 1
         
         # curriculum loss is maximum value between loss 1 and 2
         curriculum_loss_1 = torch.dot(v, l)

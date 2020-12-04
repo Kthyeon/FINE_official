a496ab8db767d3f541f8aecc55c4a84bd9ab1860
Kthyeon <potter32@kaist.ac.kr>
2020-12-04 23:09:01 +0900

    asym bash fiel modified

--- ELR/asym_baseline_c10.sh
+++ ELR/asym_baseline_c10.sh
@@ -6,27 +6,35 @@ for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar10_gce.json \
                     --percent $noise \
-                    --run_name "cifar10_baseline_gce_asym_$noise"
+                    --run_name "cifar10_baseline_gce_asym_$noise" \
+                    -d 1 \
+                    --asym 1
 done
 
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar10_sce.json --percent $noise
                     --percent $noise \
-                    --run_name "cifar10_baseline_sce_asym_$noise"
+                    --run_name "cifar10_baseline_sce_asym_$noise" \
+                    -d 1
+                    --asym 1
 done
 
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar10_elr_asym.json --percent $noise
                     --percent $noise \
-                    --run_name "cifar10_baseline_elr_asym_$noise"
+                    --run_name "cifar10_baseline_elr_asym_$noise" \
+                    -d 1 \
+                    --asym 1
 done
 
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar10_tgce.json --percent $noise
                     --percent $noise \
-                    --run_name "cifar10_baseline_tgce_asym_$noise"
+                    --run_name "cifar10_baseline_tgce_asym_$noise" \
+                    -d 1 \
+                    --asym 1
 done
     
--- ELR/asym_baseline_c100.sh
+++ ELR/asym_baseline_c100.sh
@@ -6,27 +6,31 @@ for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_gce.json \
                     --percent $noise \
-                    --run_name "cifar100_baseline_gce_asym_{$noise}"
+                    --run_name "cifar100_baseline_gce_asym_{$noise}" \
+                    --asym 1
 done
 
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_sce.json --percent $noise
                     --percent $noise \
-                    --run_name "cifar100_baseline_sce_asym_$noise"
+                    --run_name "cifar100_baseline_sce_asym_$noise" \
+                    --asym 1
 done
 
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_elr.json --percent $noise
                     --percent $noise \
-                    --run_name "cifar100_baseline_elr_asym_$noise"
+                    --run_name "cifar100_baseline_elr_asym_$noise" \
+                    --asym 1
 done
 
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_tgce.json --percent $noise
                     --percent $noise \
-                    --run_name "cifar100_baseline_tgce_asym_$noise"
+                    --run_name "cifar100_baseline_tgce_asym_$noise" \
+                    --asym 1
 done
     

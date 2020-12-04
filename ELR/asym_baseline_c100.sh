240806a3dc0951443d3c2313149f1eacc9399cb7
jongwooko <jongwoo.ko@kaist.ac.kr>
2020-12-05 03:23:51 +0900

    baseline asym gpu device 1

--- ELR/asym_baseline_c100.sh
+++ ELR/asym_baseline_c100.sh
@@ -5,6 +5,7 @@ noise_list="0.1 0.2 0.3 0.4"
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_gce.json \
+                    -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_gce_asym_{$noise}" \
                     --asym 1
@@ -13,6 +14,7 @@ done
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_sce.json --percent $noise
+                    -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_sce_asym_$noise" \
                     --asym 1
@@ -21,6 +23,7 @@ done
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_elr.json --percent $noise
+                    -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_elr_asym_$noise" \
                     --asym 1
@@ -29,6 +32,7 @@ done
 for noise in $noise_list
 do
     python train.py -c ./hyperparams/multistep/config_cifar100_tgce.json --percent $noise
+                    -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_tgce_asym_$noise" \
                     --asym 1

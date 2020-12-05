892692be58d9c27edecf09db1ddd555b95b7dbcd
jongwooko <jongwoo.ko@kaist.ac.kr>
2020-12-05 15:53:07 +0900

    change subtle code for name entity error

--- ELR/asym_baseline_c100.sh
+++ ELR/asym_baseline_c100.sh
@@ -2,18 +2,18 @@
 
 noise_list="0.1 0.2 0.3 0.4"
 
-for noise in $noise_list
-do
-    python train.py -c ./hyperparams/multistep/config_cifar100_gce.json \
-                    -d 1 \
-                    --percent $noise \
-                    --run_name "cifar100_baseline_gce_asym_{$noise}" \
-                    --asym 1
-done
+# for noise in $noise_list
+# do
+#     python train.py -c ./hyperparams/multistep/config_cifar100_gce.json \
+#                     -d 1 \
+#                     --percent $noise \
+#                     --run_name "cifar100_baseline_gce_asym_{$noise}" \
+#                     --asym 1
+# done
 
 for noise in $noise_list
 do
-    python train.py -c ./hyperparams/multistep/config_cifar100_sce.json --percent $noise
+    python train.py -c ./hyperparams/multistep/config_cifar100_sce.json \
                     -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_sce_asym_$noise" \
@@ -22,7 +22,7 @@ done
 
 for noise in $noise_list
 do
-    python train.py -c ./hyperparams/multistep/config_cifar100_elr.json --percent $noise
+    python train.py -c ./hyperparams/multistep/config_cifar100_elr.json \
                     -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_elr_asym_$noise" \
@@ -31,7 +31,7 @@ done
 
 for noise in $noise_list
 do
-    python train.py -c ./hyperparams/multistep/config_cifar100_tgce.json --percent $noise
+    python train.py -c ./hyperparams/multistep/config_cifar100_tgce.json \
                     -d 1 \
                     --percent $noise \
                     --run_name "cifar100_baseline_tgce_asym_$noise" \

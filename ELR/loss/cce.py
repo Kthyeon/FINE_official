c222452ae427ab9c51128940191baedb7dd5b342
jongwooko <jongwoo.ko@kaist.ac.kr>
2021-01-04 20:37:52 +0900

    fix small bug

--- ELR/loss/cce.py
+++ ELR/loss/cce.py
@@ -4,9 +4,9 @@ import torch.nn.functional as F
 import math
 import torch.nn as nn
 
-class CCELoss(torch.nn.Module):
+class CCELoss(nn.Module):
     def __init__(self):
         super(CCELoss, self).__init__()
         
-    def forward(self, output, target):
+    def forward(self, output, target, index):
         return F.cross_entropy(output, target)

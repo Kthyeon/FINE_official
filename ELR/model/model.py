2aa16edb20c98abd276367a092419e8dcdd1290e
Kthyeon <potter32@kaist.ac.kr>
2021-01-04 14:57:13 +0900

    add models

--- ELR/model/model.py
+++ ELR/model/model.py
@@ -1,13 +1,18 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
-from .ResNet_Zoo import ResNet, BasicBlock
+from .ResNet_Zoo import ResNet, BasicBlock, Bottleneck
 
 
+
+def resnet18(num_classes=10):
+    return ResNet(BasicBlock, [2,2,2,2], num_classes=num_classes)
 
 def resnet34(num_classes=10):
     return ResNet(BasicBlock, [3,4,6,3], num_classes=num_classes)
 
+def resnet50(num_classes=10):
+    return ResNet(Bottleneck, [3,4,6,3], num_classes=num_classes)
 
-
-
+def resnet101(num_classes=10):
+    return ResNet(Bottleneck, [3,4,23,3], num_classes=num_classes)

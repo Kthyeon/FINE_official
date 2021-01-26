8756f3d8e920f342cfd3bc7b3078972c76bc68e9
sangwook <sangwookcho@kaist.ac.kr>
2021-01-27 00:38:50 +0900

    [Add]data_loader

--- ELR/data_loader/data_loaders.py
+++ ELR/data_loader/data_loaders.py
@@ -4,6 +4,7 @@ from torchvision import datasets, transforms
 from base import BaseDataLoader
 from data_loader.cifar10 import get_cifar10
 from data_loader.cifar100 import get_cifar100
+from data_loader.clothing1m import get_clothing
 from parse_config import ConfigParser
 from PIL import Image
 
@@ -68,5 +69,37 @@ class CIFAR100DataLoader(BaseDataLoader):
         super().__init__(self.train_dataset, batch_size, shuffle, validation_split, num_workers, pin_memory,
                          val_dataset = self.val_dataset)
     def run_loader(self, batch_size, shuffle, validation_split, num_workers, pin_memory):
+        super().__init__(self.train_dataset, batch_size, shuffle, validation_split, num_workers, pin_memory,
+                         val_dataset = self.val_dataset)
+
+
+class Clothing1MDataLoader(BaseDataLoader):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_batches=0, training=True, num_workers=4, pin_memory=True):
+
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.num_batches = num_batches
+        self.training = training
+
+        self.transform_train = transforms.Compose([
+                transforms.Resize(256),
+                transforms.RandomCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),                
+                transforms.Normalize((0.6959, 0.6537, 0.6371),(0.3113, 0.3192, 0.3214)),                     
+            ]) 
+        self.transform_val = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize((0.6959, 0.6537, 0.6371),(0.3113, 0.3192, 0.3214)),
+            ])     
+
+        self.data_dir = data_dir
+        config = ConfigParser.get_instance()
+        cfg_trainer = config['trainer']
+        self.train_dataset, self.val_dataset = get_clothing(config['data_loader']['args']['data_dir'], cfg_trainer, num_samples=self.num_batches*self.batch_size, train=training,
+                transform_train=self.transform_train, transform_val=self.transform_val)
+
         super().__init__(self.train_dataset, batch_size, shuffle, validation_split, num_workers, pin_memory,
                          val_dataset = self.val_dataset)

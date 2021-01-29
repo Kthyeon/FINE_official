b1fe53c774eb4e871acb180654f946ee88a2a478
jaychoi12 <jaychoi@postech.ac.kr>
2021-01-29 16:01:31 +0900

    add bash files for initial exp.

--- DivideMix/initial_cifar10.sh
+++ DivideMix/initial_cifar10.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python Train_cifar_robustloss.py --gpuid 1 --noise_mode asym --r 0.4 --distill initial --refinement --lambda_u 0
-python Train_cifar_robustloss.py --gpuid 1 --noise_mode sym --r 0.2 --distill initial --refinement --lambda_u 0
-python Train_cifar_robustloss.py --gpuid 1 --noise_mode sym --r 0.8 --distill initial --refinement --lambda_u 25
+python Train_cifar_robustloss.py --gpuid 1 --noise_mode asym --r 0.4 --distill initial --teacher_model pretrained/multistep_asym_40_elr.pth --lambda_u 0
+python Train_cifar_robustloss.py --gpuid 1 --noise_mode sym --r 0.2 --distill initial --teacher_model pretrained/multistep_asym_40_elr.pth --refinement --lambda_u 0
+python Train_cifar_robustloss.py --gpuid 1 --noise_mode sym --r 0.8 --distill initial --teacher_model pretrained/multistep_asym_40_elr.pth --refinement --lambda_u 25

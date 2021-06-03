078ab9864a40417510adf21715c0127e75e7a075
Kthyeon <potter32@kaist.ac.kr>
2021-06-03 16:26:56 +0900

    F-dividemix scripts

--- dividemix/scripts/refinement_dynamic_cifar10.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# For CLK
-python Train_cifar.py --gpuid 1 --noise_mode sym --r 0.2 --distill dynamic --refinement --lambda_u 0 --distill_mode kmeans
-python Train_cifar.py --gpuid 1 --noise_mode sym --r 0.8 --distill dynamic --refinement --lambda_u 25 --distill_mode kmeans
-
-# For SAME
-python Train_cifar.py --gpuid 1 --noise_mode sym --r 0.2 --distill dynamic --refinement --lambda_u 0 --distill_mode eigen
-python Train_cifar.py --gpuid 1 --noise_mode sym --r 0.8 --distill dynamic --refinement --lambda_u 25 --distill_mode eigen
